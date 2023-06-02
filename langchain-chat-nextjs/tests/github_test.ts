8799bd43829bb2be1b930130faea7e11ac46e469
Ryuta Sakamoto <106237293+student-ops@users.noreply.github.com>
2023-06-02 07:21:04 +0000

    works with llama

--- langchain-chat-nextjs/tests/github_test.ts
+++ langchain-chat-nextjs/tests/github_test.ts
@@ -2,23 +2,23 @@ import { OpenAI } from "langchain/llms/openai";
 import { BufferMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains";
 import { GithubRepoLoader } from "langchain/document_loaders/web/github";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+
 async function main() {
   const loader = new GithubRepoLoader(
     "https://github.com/hwchase17/langchainjs",
     { branch: "main", recursive: false, unknown: "warn" }
   );
   const docs = await loader.load();
+  const chat = new OpenAI({ temperature: 0 });
+  const memory = new BufferMemory();
+  // const chain = new ConversationChain({ llm: chat, memory: memory });
   console.log(docs)
   
-  // const model = new OpenAI({});
-  // const memory = new BufferMemory({ memoryKey: "chat_history", returnMessages: true});
-  // const chain = new ConversationChain({ llm: model, memory: memory });
-  // const res1 = await chain.call({ input: "Hi! I'm a chatbot." });
-  // console.log(res1);
-  
-  // const res2 = await chain.call({ input: "How can I help you?" });
-  // console.log(res2);
+  // Now you can start a conversation with the chatbot by calling the chain's `call` method with a user input.
+  // const message = new HumanChatMessage(docs);
+  // const response = await chat.call([message]);
+
 }
 
-
-main()
+main();
